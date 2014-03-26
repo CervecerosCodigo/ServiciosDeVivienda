118456faa9dea5502fa6805de3e8ea53c8480a78
Lukas Larsed <git@lukaslarsed.com>
2014-03-26 15:50:04 +0100

    Oprettet prosjektstruktur for NetBeans i master branchen

---
+++ src/serviciosdevivienda/ServiciosDeVivienda.java
@@ -0,0 +1,17 @@
+
+package serviciosdevivienda;
+
+/**
+ * 
+ * File: ServiciosDeVivienda.java
+ * Project: ServiciosDeVivienda
+ * Mar 26, 2014
+ * @author Lukas David Larsed, s198569@stud.hioa.no
+ */
+public class ServiciosDeVivienda {
+
+    public static void main(String[] args) {
+
+    }
+
+}
