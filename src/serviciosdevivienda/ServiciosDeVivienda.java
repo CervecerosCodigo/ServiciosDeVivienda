6e0fa3ea4c02b0ec325fc00ad3b1755f4ab8f497
Lukas Larsed <git@lukaslarsed.com>
2014-04-08 22:05:23 +0200

    main() oppdatert med to st runnable() metoder som brukes ved oppstart av GUI og lukking. Den som kjører ved avlutting skal brukes til å serialisere fra main() dersom det lar seg å gjøre.

--- src/serviciosdevivienda/ServiciosDeVivienda.java
+++ src/serviciosdevivienda/ServiciosDeVivienda.java
@@ -1,26 +1,33 @@
-
 package serviciosdevivienda;
 
 import controller.*;
-import view.*;
-import register.*;
-import java.util.*;
+import javax.swing.SwingUtilities;
 
 /**
- * 
- * File: ServiciosDeVivienda.java
- * Project: ServiciosDeVivienda
- * Mar 26, 2014
+ *
+ * File: ServiciosDeVivienda.java Project: ServiciosDeVivienda Mar 26, 2014
+ *
  * @author Lukas David Larsed, s198569@stud.hioa.no
  */
 public class ServiciosDeVivienda {
 
     public static void main(String[] args) {
 
-        //VelkomstFrame oppstartsvindu = new VelkomstFrame();
-        //MainController controller = new MainController(oppstartsvindu );
-        MainController controller = new MainController();
+        SwingUtilities.invokeLater(new Runnable() {
 
+            @Override
+            public void run() {
+
+                MainController controller = new MainController();
+
+            }
+        });
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("Programmet avsluttes");
+            }
+        }));
     }
-
 }
