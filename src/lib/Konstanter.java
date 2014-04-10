e8ef39a1b531b7a650c25b3a4c50391b6bded680
Lukas Larsed <git@lukaslarsed.com>
2014-04-10 13:16:50 +0200

    Nye konstanter

--- src/lib/Konstanter.java
+++ src/lib/Konstanter.java
@@ -15,4 +15,8 @@ public class Konstanter {
             + "<A,a<B,b<C,c<D,d<E,e<F,f<G,g<H,h<I,i<J,j"
             + "<K,k<L,l<M,m<N,n<O,o<P,p<Q,q<R,r<S,s<T,t"
             + "<U,u<V,v<W,w<X,x<Y,y<Z,z<Æ,æ<Ø,ø<Å=AA,å=aa;AA,aa";
+    
+    public static final long SERNUM = 1234L;
+    
+    public static final String FILNANV = "programdata/data.iso";
 }
