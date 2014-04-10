0055937e993e7d3f1846ee8733d36ea2fb2018e0
Lukas Larsed <git@lukaslarsed.com>
2014-04-10 13:20:00 +0200

    Lagt til interfacet Serializable (ikke helt sikker på dersom det trengs)

--- src/register/Register.java
+++ src/register/Register.java
@@ -1,6 +1,7 @@
 package register;
 //Laget av Espen Zaal, studentnummer 198599 i klasse Informasjonsteknologi.
 
+import java.io.Serializable;
 import java.util.*;
 
 /**
@@ -9,7 +10,7 @@ import java.util.*;
  * @author espen
  * @param <T> 
  */
-public abstract class Register<T> {
+public abstract class Register<T> implements Serializable{
 
     protected Collection<T> collection;
 
