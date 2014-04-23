cbd4dbf96aae5ab44d0ad5db70e61fecbe2dbc60
Espen Zaal <espenbz@gmail.com>
2014-04-23 10:06:09 +0200

    Små endringer i Controller for HTML, for å få bort den irriterende melgingen..

--- src/controller/ControllerTabellOgOutput.java
+++ src/controller/ControllerTabellOgOutput.java
@@ -364,7 +364,7 @@ public class ControllerTabellOgOutput {
         
         BildeFilSti bildefilsti = new BildeFilSti();
         String localImageSrc = bildefilsti.getBoligFremsideBildeHTML(skalVises);
-        new Melding("Melding", "Kommenter vekk linje 374 i ControllerTabellOutput.java for å få vekk denne\n Skal fjerne meldingen etter at ha testet BildeFilSti på Windows.\n\n"+localImageSrc); 
+        //new Melding("Melding", "Kommenter vekk linje 374 i ControllerTabellOutput.java for å få vekk denne\n Skal fjerne meldingen etter at ha testet BildeFilSti på Windows.\n\n"+localImageSrc); 
         
         html.append("<table id='bildetabell'>");
         html.append("<tr id='bilderad'>");
@@ -523,7 +523,11 @@ public class ControllerTabellOgOutput {
         html.append("</table>");
         //html.append("<hr class='linjeEn'>");
         
-        String localImageSrc = ControllerTabellOgOutput.class.getClassLoader().getSystemResource("77_1655132553.jpg").toString();
+//        String localImageSrc = ControllerTabellOgOutput.class.getClassLoader().getSystemResource("77_1655132553.jpg").toString();
+        
+        BildeFilSti bildefilsti = new BildeFilSti();
+        String localImageSrc = bildefilsti.getBoligFremsideBildeHTML(skalVises.getBolig());
+        //new Melding("Melding", "Kommenter vekk linje 374 i ControllerTabellOutput.java for å få vekk denne\n Skal fjerne meldingen etter at ha testet BildeFilSti på Windows.\n\n"+localImageSrc);         
         
         html.append("<table id='bildetabell'>");
         
--- src/view/TopPanelMegler.java
+++ src/view/TopPanelMegler.java
@@ -10,26 +10,26 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 public class TopPanelMegler extends AbstractPanel {
-	
-	private MeglerRadioKnapper meglerRadioKnapper;
-	private CustomJTextField sokeFelt;
-	private JButton sokeKnapp, lagNyKnapp;
-    
-    TopPanelMegler( String borderTitle, int dimHeight, int dimWidth ) {
+
+    private MeglerRadioKnapper meglerRadioKnapper;
+    private CustomJTextField sokeFelt;
+    private JButton sokeKnapp, lagNyKnapp;
+
+    TopPanelMegler(String borderTitle, int dimHeight, int dimWidth) {
 
         super(borderTitle, dimHeight, dimWidth);
         meglerRadioKnapper = new MeglerRadioKnapper();
-		sokeFelt = new CustomJTextField("Søk", "", 22);
-		sokeKnapp = new JButton("Søk");
-		lagNyKnapp = new JButton("Lag ny");
-		
-		setLayout(new FlowLayout(FlowLayout.LEADING, 40, 0));
-		setVisible(true);
-		
-		add(meglerRadioKnapper);
-		add(sokeFelt);
-		add(sokeKnapp);
-		add(lagNyKnapp);
+        sokeFelt = new CustomJTextField("Søk", "", 22);
+        sokeKnapp = new JButton("Søk");
+        lagNyKnapp = new JButton("Lag ny");
+
+        setLayout(new FlowLayout(FlowLayout.LEADING, 40, 0));
+        setVisible(true);
+
+        add(meglerRadioKnapper);
+        add(sokeFelt);
+        add(sokeKnapp);
+        add(lagNyKnapp);
     }
 
     public MeglerRadioKnapper getMeglerRadioKnapper() {
@@ -47,7 +47,8 @@ public class TopPanelMegler extends AbstractPanel {
     public JButton getLagNyKnapp() {
         return lagNyKnapp;
     }
-   public JRadioButton getSoknaderRadio() {
+
+    public JRadioButton getSoknaderRadio() {
         return meglerRadioKnapper.getSoknaderRadio();
     }
 
@@ -65,50 +66,50 @@ public class TopPanelMegler extends AbstractPanel {
 
     public JRadioButton getLeietakereRadio() {
         return meglerRadioKnapper.getLeietakereRadio();
-    }    
+    }
 
     public void leggTilRadioLytter(ActionListener lytter) {
         meglerRadioKnapper.addRadioListener(lytter);
     }
 
-    public void leggTilKnappeLytter(ActionListener lytter){
+    public void leggTilKnappeLytter(ActionListener lytter) {
         lagNyKnapp.addActionListener(lytter);
         sokeKnapp.addActionListener(lytter);
-    }    
-    
+    }
+
 }
 
 class MeglerRadioKnapper extends JPanel {
-	
-	private JRadioButton soknaderRadio, annonserRadio, boligerRadio, utleiereRadio, leietakereRadio;
-	private ButtonGroup radioGroup;
-	
-	public MeglerRadioKnapper() {
-		soknaderRadio = new JRadioButton("Søknader");
-		annonserRadio = new JRadioButton("Annonser");
-		boligerRadio = new JRadioButton("Boliger");
-		utleiereRadio = new JRadioButton("Utleiere");
-		leietakereRadio = new JRadioButton("Leietakere");
-		radioGroup = new ButtonGroup();
-		
-		radioGroup.add(soknaderRadio);
-		radioGroup.add(annonserRadio);
-		radioGroup.add(boligerRadio);
-		radioGroup.add(utleiereRadio);
-		radioGroup.add(leietakereRadio);
-		
-		setVisible(true);
-		setPreferredSize(new Dimension(90,110));
-		setLayout(new FlowLayout(FlowLayout.LEFT, 0, -1));
-		
-		add(soknaderRadio);
-		add(annonserRadio);
-		add(boligerRadio);
-		add(utleiereRadio);
-		add(leietakereRadio);
-		
-		soknaderRadio.setSelected(true);
-	}
+
+    private JRadioButton soknaderRadio, annonserRadio, boligerRadio, utleiereRadio, leietakereRadio;
+    private ButtonGroup radioGroup;
+
+    public MeglerRadioKnapper() {
+        soknaderRadio = new JRadioButton("Søknader");
+        annonserRadio = new JRadioButton("Annonser");
+        boligerRadio = new JRadioButton("Boliger");
+        utleiereRadio = new JRadioButton("Utleiere");
+        leietakereRadio = new JRadioButton("Leietakere");
+        radioGroup = new ButtonGroup();
+
+        radioGroup.add(soknaderRadio);
+        radioGroup.add(annonserRadio);
+        radioGroup.add(boligerRadio);
+        radioGroup.add(utleiereRadio);
+        radioGroup.add(leietakereRadio);
+
+        setVisible(true);
+        setPreferredSize(new Dimension(90, 110));
+        setLayout(new FlowLayout(FlowLayout.LEFT, 0, -1));
+
+        add(soknaderRadio);
+        add(annonserRadio);
+        add(boligerRadio);
+        add(utleiereRadio);
+        add(leietakereRadio);
+
+        soknaderRadio.setSelected(true);
+    }
 
     public JRadioButton getSoknaderRadio() {
         return soknaderRadio;
@@ -130,13 +131,12 @@ class MeglerRadioKnapper extends JPanel {
         return leietakereRadio;
     }
 
-       
-   public void addRadioListener(ActionListener lytter) {
+    public void addRadioListener(ActionListener lytter) {
         boligerRadio.addActionListener(lytter);
         utleiereRadio.addActionListener(lytter);
         leietakereRadio.addActionListener(lytter);
         soknaderRadio.addActionListener(lytter);
         annonserRadio.addActionListener(lytter);
 
-    }    
+    }
 }
