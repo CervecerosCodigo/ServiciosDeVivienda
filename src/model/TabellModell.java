ff4b00b84e9d32807b0e4f04c0cefa877e27360d
Espen Zaal <espenbz@gmail.com>
2014-04-26 18:04:51 +0200

    Endret tabellstruktur og metodene som kaller på tabellen. Det er nå én tabellmodell for hver objekttype slik at man enklere kan implementere formatering og sortering av tabellen.

---
+++ src/model/TabellModell.java
@@ -0,0 +1,54 @@
+package model;
+//Laget av Espen Zaal, studentnummer 198599 i klasse Informasjonsteknologi.
+
+import javax.swing.table.AbstractTableModel;
+
+public abstract class TabellModell extends AbstractTableModel {
+
+    Object[] mottattArray;
+    String[] overskrift;
+
+    public TabellModell() {
+        
+    }
+
+
+    public void fyllTabellMedInnhold(Object[] liste) {
+        this.mottattArray = liste;
+    }
+
+    @Override
+    public int getRowCount() {
+        return mottattArray.length;
+    }
+
+    /**
+     * Returnerer antall kolonner i tabellen.
+     * @return 
+     */
+    @Override
+    public int getColumnCount() {
+        return overskrift.length;
+    }
+
+    @Override
+    public String getColumnName(int index) {
+        return overskrift[index];
+    }
+    
+    
+    @Override
+    public Class getColumnClass(int c){
+        if( mottattArray.length > 0){
+            return getValueAt(0,c).getClass();
+        }
+        return null;
+    }
+
+    @Override
+    public Object getValueAt(int rad, int kolonne) {
+        
+        return null;
+    }
+    
+}
--- src/view/VenstrePanel.java
+++ src/view/VenstrePanel.java
@@ -1,14 +1,16 @@
 package view;
 //Laget av Espen Zaal, studentnummer 198599 i klasse Informasjonsteknologi.
 
+import model.*;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
 /**
- * Denne klassen er et JPanel (arvet) som inneholder tabellen.
- * Den definerer Tabellen, samt oppretter TabellModell, som definerer 
- * selve funksjonaliteten til tabellen.
+ * Denne klassen er et JPanel (arvet) som inneholder tabellen. Den definerer
+ * Tabellen, samt oppretter TabellModell, som definerer selve funksjonaliteten
+ * til tabellen.
+ *
  * @author espen
  */
 public class VenstrePanel extends AbstractPanel {
@@ -21,55 +23,30 @@ public class VenstrePanel extends AbstractPanel {
         super(borderTitle, dimHeight, dimWidth);
         setLayout(new GridLayout(1, 1));
 
-        tabellModell = new TabellModell();
-        tabell = new JTable(tabellModell);
-        sorterer = new TableRowSorter<>( tabellModell );
-        
-        
+        tabell = new JTable();
         tabell.setFillsViewportHeight(true);
-        
         add(new JScrollPane(tabell));
 
     }//End Constructor
 
     /**
-     * Returnerer tabellmodellen som definerer tabellens innhold og funksjon.
-     * @return
-     */
-    public TabellModell getTabellModell() {
-        return tabellModell;
-    }
-    
-    public void settTabellSortering(){
-        tabell.setRowSorter(sorterer);
-    }
-
-    /**
-     * Setter kolonnebredden etter innholdet i tabellen. 
-     * Sammen med tabellens Auto-resize så blir tabellen fyllt ut i maks bredde,
-     * men samtidig med rett kolonnebredde.
-     */
-    public void resizeKolonneBredde() {
-        final TableColumnModel kolonneModell = tabell.getColumnModel();
-        Component comp = null;
-        for (int kol = 0; kol < tabell.getColumnCount(); kol++) {
-            int bredde = 50; //minste bredde
-            for (int rad = 0; rad < tabell.getRowCount(); rad++) {
-                TableCellRenderer renderer = tabell.getCellRenderer(rad, kol);
-                comp = tabell.prepareRenderer(renderer, rad, kol);
-                bredde = Math.max(comp.getPreferredSize().width, bredde);
-            }
-            kolonneModell.getColumn(kol).setPreferredWidth(bredde);
-        }
-    }
-    /**
      * Returnerer tabellen.
+     *
      * @return
      */
     public JTable getTable() {
         return tabell;
     }
 
+    public TableRowSorter<TabellModell> getSorterer() {
+        return sorterer;
+    }
+    
+    public void settTabellSortering() {
+        tabell.setRowSorter(sorterer);
+    }
 
-
+    public void setSorterer(TableRowSorter<TabellModell> sorterer) {
+        this.sorterer = sorterer;
+    }
 }
