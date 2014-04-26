502b11bd35311e538aa53b5817e46b0dc4fdc619
Lukas Larsed <git@lukaslarsed.com>
2014-04-26 22:11:23 +0200

    BildeFilsti Når man opretter en ny annonse vil det bli kopiert nå et standard bilde fra "programdata/img/default/1.jpg" for alle boliger som ikke får bilde fra brukeren med enn gang. Dette er kun til for dersom man ikke laster opp egne bilder for boligen så unngår vi å ha en tom boks i utskriften.

--- src/lib/BildeFilSti.java
+++ src/lib/BildeFilSti.java
@@ -1,5 +1,14 @@
 package lib;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import model.Bolig;
@@ -12,6 +21,32 @@ import model.Bolig;
  * @author Lukas David Larsed, s198569@stud.hioa.no
  */
 public class BildeFilSti {
+
+//    InputStream instream;
+//    OutputStream outstream;
+    public BildeFilSti() {
+    }
+
+    /**
+     * Returnerer an absolute filsti til alle bildegallerier for boliger.
+     *
+     * @return String med filsti
+     */
+    public String getAbsoluteGalleryPath() {
+        Path currentPath = Paths.get("");
+        String filsti = currentPath.toAbsolutePath().toString();
+        return filsti + "/" + Konstanter.BILDEGALLERIER + "/";
+    }
+
+    /**
+     * Returnerer filsti til standardbilde ved oprettelse av ny bolig. Denne
+     * brukes dersom brukeren ikke laster opp egne bilder.
+     *
+     * @return String med filsti
+     */
+    public String getAbsolutePathToStandardBilde() {
+        return getAbsoluteGalleryPath() + "default" + "/" + "1.jpg";
+    }
 
     /**
      * Returnerer den absolute filstien til posisjonen med bildegallerier (skal
@@ -59,5 +94,75 @@ public class BildeFilSti {
      */
     public String getBoligFremsideBildeHTML(Bolig bolig) {
         return "file:" + getBoligFremsideBilde(bolig);
+    }
+
+    /**
+     * Lager en gallerimappe for en ny bolig og kopierer inn standard bilde.
+     * Standardbilde blir erstattet etter at brukeren legger til eget bilde(r)
+     * for boligen.
+     *
+     * @param bolig
+     */
+    public void lagBildemappeForBolig(Bolig bolig) {
+        File filsti = new File(getBoligGalleryPath(bolig));
+        try {
+            filsti.mkdirs();
+        } catch (SecurityException se) {
+            Melding.visMelding("Sikkerhetsvarning", "Mislykket med å oprette gallerimappe\n" + se.getMessage());
+        } finally {
+            kopierStandardBilde(bolig);
+//            kopierStandardBilde2(bolig);
+        }
+    }
+
+    /**
+     * Kopierer standard bilde for en annonse som ikke fått et bilde. Da vil det
+     * bli hentet et standard bilde fra mappe programdata/img/default/1.jpg
+     *
+     * @param bolig
+     */
+    private void kopierStandardBilde(Bolig bolig) {
+        File kildeFil = new File(getAbsolutePathToStandardBilde());
+        File destinasjonsFil = new File(getBoligGalleryPath(bolig) + "/" + "1.jpg");
+
+        try {
+            Files.copy(kildeFil.toPath(), destinasjonsFil.toPath());
+        } catch (IOException ex) {
+            Melding.visMelding("kopierStandardBilde()", ex.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Alternativ metode for å kopiere standard bildefil. Kan brukes dersom den
+     * første metoden vil får problem.
+     *
+     * @param bolig
+     */
+    private void kopierStandardBilde2(Bolig bolig) {
+        InputStream instream = null;
+        OutputStream outstream = null;
+
+        try {
+            File kildeFil = new File(getAbsolutePathToStandardBilde());
+            File destinasjonsFil = new File(getBoligGalleryPath(bolig) + "/" + "1.jpg");
+
+            instream = new FileInputStream(kildeFil);
+            outstream = new FileOutputStream(destinasjonsFil);
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = instream.read(buffer)) > 0) {
+                outstream.write(buffer, 0, length);
+            }
+
+            instream.close();
+            outstream.close();
+
+        } catch (FileNotFoundException e) {
+            Melding.visMelding("kopierStandardBilde2()", "Finne ikke fil: \n" + e.getMessage());
+        } catch (IOException f) {
+            Melding.visMelding("kopierStandardBilde2()", "IO Feil: \n" + f.getMessage());
+        }
     }
 }
