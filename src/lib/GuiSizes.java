19467223c9f1cc23ff43d0c0c91c25b433eda525
Lukas Larsed <git@lukaslarsed.com>
2014-05-02 14:30:26 +0200

    GuiSizes En klasse med konstanter som kan brukes for å sette størrelse på GUI komponenter.

---
+++ src/lib/GuiSizes.java
@@ -0,0 +1,22 @@
+
+package lib;
+
+/**
+ * En klasse med konstanter som bestemmer størrelse på GUI komponenter.
+ * File: GuiSizes.java
+ Package: lib
+ Project: ServiciosDeVivienda
+ May 2, 2014
+ * @author Lukas David Larsed, s198569@stud.hioa.no
+ */
+public class GuiSizes {
+
+    /**
+     * Dette er vår mediumstørrelse for en textfield.
+     */
+    public static final int FIELD_MEDIUM = 15;
+    
+    public static final int TEXTAREA_ROW_MEDIUM = 6;
+    public static final int TEXTAREA_COL_MEDIUM = 16;
+   
+}
