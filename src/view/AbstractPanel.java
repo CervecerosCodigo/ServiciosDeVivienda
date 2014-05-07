f1112134a01b66803be6865b57d0630e5b5b68b8
Lukas Larsed <git@lukaslarsed.com>
2014-05-07 10:44:34 +0200

    Abstrakte Paneler Har lagt til konstruktører slik at vi kan instnsiere panelene uten borders, kun med størrelse eller kun med en layoutmanager.

--- src/view/AbstractPanel.java
+++ src/view/AbstractPanel.java
@@ -13,6 +13,12 @@ public abstract class AbstractPanel extends JPanel{
     private int dimHeight;
     private int dimWidth;
     
+    /**
+     * Abstract panel med en tittelborder.
+     * @param borderTitle
+     * @param dimHeight
+     * @param dimWidth 
+     */
     public AbstractPanel( String borderTitle, int dimHeight, int dimWidth ) {
 
         this.borderTitle = borderTitle;
@@ -29,7 +35,24 @@ public abstract class AbstractPanel extends JPanel{
         Border innerBorder = BorderFactory.createTitledBorder(this.borderTitle);
         Border outerBorder = BorderFactory.createEmptyBorder(0, 0, 0, 0);
         setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+    }
+    
+    /**
+     * Abstract panel som blir opprettet uten border. 
+     * @param dimHeight
+     * @param dimWidth 
+     */
+    public AbstractPanel(int dimHeight, int dimWidth ) {
+
+        this.dimHeight = dimHeight;
+        this.dimWidth = dimWidth;
         
+        Dimension dim = getPreferredSize();
+        dim.height = this.dimHeight;
+        dim.width = this.dimWidth;
+        setPreferredSize(dim);
+        
+        setBackground(Konstanter.BAKGRUNNSFARGEPANEL);
     }
     
     public AbstractPanel(){
--- src/view/registrer/CustomSubPanel.java
+++ src/view/registrer/CustomSubPanel.java
@@ -10,20 +10,59 @@ public class CustomSubPanel extends AbstractPanel {
 
     private LayoutManager layout;
 
+    /**
+     * Border og størrelse.
+     * @param borderNavn
+     * @param hoyde
+     * @param bredde 
+     */
     public CustomSubPanel(String borderNavn, int hoyde, int bredde) {
         super(borderNavn, hoyde, bredde);
         setLayout(new GridLayout(1, 1));
         setVisible(true);
     }
 
+    /**
+     * Med layout manager.
+     * @param layout 
+     */
     public CustomSubPanel(LayoutManager layout) {
         setLayout(layout);
         setVisible(true);
     }
     
+    /**
+     * Border med navn, størrelse og layoutmanager.
+     * @param borderNavn
+     * @param hoyde
+     * @param bredde
+     * @param layout 
+     */
     public CustomSubPanel(String borderNavn, int hoyde, int bredde, LayoutManager layout) {
         super(borderNavn, hoyde, bredde);
         setLayout(layout);
+        setVisible(true);
+    }
+    
+    /**
+     * Størrelse og layoutmanager.
+     * @param hoyde
+     * @param bredde
+     * @param layout 
+     */
+    public CustomSubPanel(int hoyde, int bredde, LayoutManager layout) {
+        super(hoyde, bredde);
+        setLayout(layout);
+        setVisible(true);
+    }
+
+    /**
+     * Størrelse, uten layout og borders.
+     * @param hoyde
+     * @param bredde 
+     */
+    public CustomSubPanel(int hoyde, int bredde) {
+        super(hoyde, bredde);
         setVisible(true);
     }
 
