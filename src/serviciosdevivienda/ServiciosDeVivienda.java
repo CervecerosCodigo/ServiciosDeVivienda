bddddc0dce450777c99574caa61e7efc839e57e6
Petter Lysne <petterlysne@hotmail.com>
2014-05-07 23:55:27 +0200

    Fjernet border/divider til tabbedpane

--- src/serviciosdevivienda/ServiciosDeVivienda.java
+++ src/serviciosdevivienda/ServiciosDeVivienda.java
@@ -1,7 +1,5 @@
 package serviciosdevivienda;
 
-import controller.SkrivTilLesFraFil;
-
 import java.awt.Color;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,6 +10,7 @@ import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import lib.Konstanter;
+import controller.SkrivTilLesFraFil;
 
 /**
  *
@@ -32,6 +31,10 @@ public class ServiciosDeVivienda {
             for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     UIManager.setLookAndFeel(info.getClassName());
+                    UIManager.getLookAndFeelDefaults().put("TabbedPane:TabbedPaneTabArea[Disabled].backgroundPainter", null);
+                    UIManager.getLookAndFeelDefaults().put("TabbedPane:TabbedPaneTabArea[Enabled+MouseOver].backgroundPainter", null);
+                    UIManager.getLookAndFeelDefaults().put("TabbedPane:TabbedPaneTabArea[Enabled+Pressed].backgroundPainter", null);
+                    UIManager.getLookAndFeelDefaults().put("TabbedPane:TabbedPaneTabArea[Enabled].backgroundPainter", null);
                     break;
                 }
             }
